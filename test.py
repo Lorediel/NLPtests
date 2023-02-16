fb7d8a878dcb782815b17f19f76df4b2c9630dba
Lorediel <ld1@live.it>
2023-02-16 09:25:02 +0100

    resnet test

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from datasets import load_dataset
 from utils import splitTrainTestVal
 
 resnet = ResnetModel()
-datasets = splitTrainTestVal("/content/NLPtests/MULTI-Fake-Detective_Task1_Data.tsv")
+datasets = splitTrainTestVal("/content/NLPtests/MULTI-Fake-Detective_Task1_Data.tsv", delete_date = True)
 resnet.test(datasets)
