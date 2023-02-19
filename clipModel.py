3164a834b3e6f556726b90649ec0eb18530c5c79
Lorediel <ld1@live.it>
2023-02-19 10:59:46 +0100

    eval of clip

--- clipModel.py
+++ clipModel.py
@@ -67,37 +67,61 @@ class ClipModel:
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.model.to(self.device)
 
-    def test(self, datasets, dir_name):
+    def eval(self, ds):
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.model.to(device)
-        self.model.test()
-        eval_dataloader = torch.utils.data.DataLoader(
-            datasets["valid"], batch_size=8
+        self.model.eval()
+        dataloader = torch.utils.data.DataLoader(
+            ds, batch_size=8, collate_fn = collate_fn
         )
-        progress_bar = tqdm(range(len(eval_dataloader)))
-        for batch in eval_dataloader:
-            batch = {k: v for k, v in batch.items()}
-            image_paths = batch["Media"]
-            
-            images = []
-            images_per_row = []
-            for row_paths in image_paths:
-                number_of_row_images = 0
-                for image_path in ast.literal_eval(row_paths): 
-                    number_of_row_images += 1
-                    image = pil_loader(os.path.join(dir_name, image_path))
-                    images.append(image)
-                    images_per_row.append(number_of_row_images)
-            text = batch["Text"]
+        total_preds = []
+        total_labels = []
+        with torch.no_grad():
+            for batch in dataloader:
+                texts = batch["text"]
+                images_list = batch["images"]
+                mask = batch["images_mask"]
+                labels = batch["label"]
+
+                nums_images = []
+                for m in mask:
+                    nums_images.append(sum(m))
+                images_list = [item.to(device) for sublist, mask_sublist in zip(images_list, mask)
+                          for item, mask_value in zip(sublist, mask_sublist) 
+                          if mask_value]
+                
+                t_inputs = self.model.processor(text=texts, return_tensors="pt", padding=True, truncation=True)
+                i_inputs = self.model.processor(images = images_list, return_tensors="pt", padding=True)
+                
+                for k, v in t_inputs.items():
+                    t_inputs[k] = v.to(device)
+                for k, v in i_inputs.items():
+                    i_inputs[k] = v.to(device)
+                
+                
+                logits, probs = self.model(
+                    input_ids=t_inputs.input_ids,
+                    attention_mask=t_inputs.attention_mask,
+                    pixel_values=i_inputs.pixel_values,
+                    nums_images = nums_images,
+                )
+
+                preds = torch.argmax(logits, dim=1).detach().cpu().numpy()
+                total_preds += list(preds)
+                total_labels += list(labels)
+        metrics = compute_metrics(total_labels, total_preds)
+        return metrics
 
 
 
-    def train(self, train_ds, lr = 5e-5, num_epochs = 3, warmup_steps = 0, save_path = None):
+    def train(self, train_ds, eval_ds, lr = 5e-5, num_epochs = 3, warmup_steps = 0, num_eval_steps = 10, save_path = "./"):
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.model.to(device)
 
         dataloader = torch.utils.data.DataLoader(
             train_ds, batch_size=8, collate_fn = collate_fn
+        )
+        eval_dataloader = torch.utils.data.DataLoader(
+            eval_ds, batch_size=8, collate_fn = collate_fn
         )
 
         criterion = nn.CrossEntropyLoss()
@@ -114,21 +138,25 @@ class ClipModel:
             num_training_steps=num_training_steps
         )
         progress_bar = tqdm(range(num_training_steps))
+        current_step = 0
+        best_eval = -1
         for epoch in range(num_epochs):
             for batch in dataloader:
+                current_step += 1
                 #batch = {k: v.to(device) for k, v in batch.items()}
                 texts = batch["text"]
                 images_list = batch["images"]
                 mask = batch["images_mask"]
                 labels = batch["label"]
 
-                # mask and flatten the list of images 
                 nums_images = []
                 for m in mask:
                     nums_images.append(sum(m))
-                images_list = [item for sublist, mask_sublist in zip(images_list, mask)
+                images_list = [item.to(device) for sublist, mask_sublist in zip(images_list, mask)
                           for item, mask_value in zip(sublist, mask_sublist) 
                           if mask_value]
+
+
 
                 
                 t_inputs = self.model.processor(text=texts, return_tensors="pt", padding=True, truncation=True)
@@ -139,6 +167,7 @@ class ClipModel:
                 for k, v in i_inputs.items():
                     i_inputs[k] = v.to(device)
                 labels = torch.tensor(labels).to(device)
+
                 nums_images = torch.tensor(nums_images).to(dtype=torch.long, device=device)
                 outputs = self.model(
                     input_ids=t_inputs.input_ids,
@@ -150,17 +179,22 @@ class ClipModel:
                 logits = outputs[0]
                 
                 preds = torch.argmax(logits, dim=1).detach().cpu().numpy()
-                metrics = compute_metrics(preds, batch["label"])
-                print(metrics)
                 loss = criterion(logits, labels)
+                if (current_step % num_eval_steps == 0):
+                    eval_metrics = self.eval(eval_dataloader)
+                    f1_score = eval_metrics["f1"]
+                    if f1_score > best_eval:
+                        self.model.save_pretrained(os.path.join(save_path, "best_model"))
+                    self.model.train()
+                
+
                 loss.backward()
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
                 
-        if save_path is not None:
-            self.model.save_pretrained(save_path)
+        
         return self.model
                 
 
