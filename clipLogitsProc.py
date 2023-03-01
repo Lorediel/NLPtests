0638b9effbe480f789bde9f4359148609c62aee2
Lorediel <ld1@live.it>
2023-03-01 15:58:40 +0100

    logits postproc

--- clipLogitsProc.py
+++ clipLogitsProc.py
@@ -3,12 +3,11 @@ import requests
 import torch
 import torch.nn as nn
 import ast
-from transformers import AdamW, get_scheduler, AutoProcessor, VisionTextDualEncoderModel, AutoTokenizer, AutoFeatureExtractor, PretrainedConfig, PreTrainedModel, CLIPProcessor
+from transformers import AdamW, get_scheduler, AutoProcessor, VisionTextDualEncoderModel, AutoTokenizer, AutoFeatureExtractor, PretrainedConfig, PreTrainedModel
 import os
 from tqdm.auto import tqdm
 from NLPtests.utils import *
 from NLPtests.FakeNewsDataset import collate_fn
-import torch.nn.functional as F
 
 
 
@@ -17,60 +16,20 @@ class Model(nn.Module):
         super(Model, self).__init__()
         
         self.base_model = VisionTextDualEncoderModel.from_pretrained("clip-italian/clip-italian")
-        self.processor = CLIPProcessor.from_pretrained("clip-italian/clip-italian")
+        self.processor = AutoProcessor.from_pretrained("clip-italian/clip-italian")
         self.tokenizer = AutoTokenizer.from_pretrained("clip-italian/clip-italian")
         self.tokenizerLast = AutoTokenizer.from_pretrained("clip-italian/clip-italian", padding_side = 'left', truncation_side = 'left')
-        self.feature_extractor = AutoFeatureExtractor.from_pretrained("openai/clip-vit-base-patch32")
         
         #self.dropout2 = nn.Dropout(0.2)
         #self.layernorm1 = nn.LayerNorm(512*2)
-        self.linear1 = nn.Linear(512*2, 768)
-        #self.linear2 = nn.Linear(768, 768)
-        self.linear3 = nn.Linear(768, 4)
+        self.tanh = nn.Tanh()
+        self.linear1 = nn.Linear(512*2, 512*2)
+        self.linear2 = nn.Linear(512*2, 512*2)
+        self.linear3 = nn.Linear(512*2, 4)
         self.relu = nn.ReLU()
-        self.layernorm = nn.LayerNorm(768)
+        self.layernorm = nn.LayerNorm(512*2)
         self.dropout = nn.Dropout(0.1)
         self.softmax = nn.Softmax(dim=1)
-
-        self.labels = [
-            "Un'immagine sicuramente falsa",
-            "Un'immagine probabilmente falsa",
-            "Un'immagine probabilmente vera",
-            "Un'immagine sicuramente vera",
-        ]
-
-        self.labels_embeddings = self.embed_texts(self.labels, self.processor, self.base_model)
-
-    def embed_texts(self, texts, processor, model):
-        inputs = processor(text=texts, padding="longest")
-        input_ids = torch.tensor(inputs["input_ids"])
-        attention_mask = torch.tensor(inputs["attention_mask"])
-        
-        with torch.no_grad():    
-            embeddings = model.get_text_features(
-                input_ids=input_ids, attention_mask=attention_mask
-            )
-        return embeddings
-
-    def embed_images(self, images, processor, model):
-        inputs = processor(images=images)
-
-        pixel_values = torch.tensor(np.array(inputs["pixel_values"]))
-
-        with torch.no_grad():
-            embeddings = model.get_image_features(pixel_values=pixel_values)
-        return embeddings
-
-    def classify_image(self, image_embeds):
-        preds = []
-        for i in range(len(image_embeds)):
-            img_embed = image_embeds[i]
-            sim = F.cosine_similarity(img_embed, self.labels_embeddings, dim=1)
-            closest = sim.argmax()
-            preds.append(closest)
-        
-       
-       
 
     def forward(self, input_ids, attention_mask, pixel_values, nums_images):
         
@@ -80,8 +39,6 @@ class Model(nn.Module):
 
         i_embeddings = self.base_model.get_image_features(pixel_values = pixel_values)
 
-
-        """
         new_te = []
         for i in range(len(nums_images)):
             current_text_embedding = t_embeddings[i]
@@ -89,30 +46,26 @@ class Model(nn.Module):
             current_text_embedding = current_text_embedding.repeat(nums_images[i], 1)
             new_te.append(current_text_embedding)
         t_embeddings = torch.cat(new_te, dim=0)
-        """
-        preds = self.classify_image(i_embeddings)
-        return preds
-        
         
         embeddings_images = i_embeddings
+        embeddings_images = self.tanh(embeddings_images)
         embeddings = torch.cat((t_embeddings, embeddings_images), dim=1)
 
         #embeddings = self.layernorm1(embeddings)
         #embeddings = self.dropout2(embeddings)
-        embeddings = self.relu(embeddings)
+        #embeddings = self.relu(embeddings)
 
         embeddings = self.linear1(embeddings)
         embeddings = self.layernorm(embeddings)
         embeddings = self.dropout(embeddings)
         embeddings = self.relu(embeddings)
 
-        #embeddings = self.linear2(embeddings)
-        #embeddings = self.layernorm(embeddings)
-        #embeddings = self.dropout(embeddings)
-        #embeddings = self.relu(embeddings)
+        embeddings = self.linear2(embeddings)
+        embeddings = self.layernorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        embeddings = self.relu(embeddings)
 
         logits = self.linear3(embeddings)
-
         #post process logits
         new_logits = []
         base = 0
@@ -122,7 +75,6 @@ class Model(nn.Module):
             new_logits.append(sample_logits)
             base += nums_images[i]
         logits = torch.cat(new_logits, dim=0)
-
         probs = self.softmax(logits)
 
         return logits, probs
@@ -140,8 +92,7 @@ def pil_loader(path: str):
     
 class ClipModel:
     #tokenizer_max_length = 512
-
-
+    
 
     def __init__(self):
         self.model = Model()
@@ -153,6 +104,35 @@ class ClipModel:
             return self.model.tokenizer(texts, return_tensors="pt", padding = True, truncation=True)
         elif tokenization_strategy == "last":
             return self.model.tokenizerLast(texts, return_tensors="pt", padding = True, truncation=True)
+        elif tokenization_strategy == "head-tail":
+            max_len = 512
+            tokens = self.model.tokenizer(texts)
+            half_len = int(max_len/2)
+            post_tokens = {
+                "input_ids": [],
+                "attention_mask": []
+            }
+            max_token_len = 0
+            for token_list in tokens.input_ids:
+                tl = len(token_list)
+                if tl>max_token_len:
+                    max_token_len = tl
+            max_len = min(max_token_len, max_len)
+            for token_list in tokens.input_ids:
+                new_tokens = []
+                tl = len(token_list)
+                if tl>max_len:
+                    new_tokens = token_list[:half_len] + token_list[-half_len:]
+                    attention_mask = [1] * max_len
+                elif tl<=max_len:
+                    # add padding
+                    new_tokens = token_list + [0] * (max_len - tl)
+                    attention_mask = [1] * tl + [0] * (max_len - tl)
+                post_tokens["input_ids"].append(new_tokens)
+                post_tokens["attention_mask"].append(attention_mask)
+            post_tokens["input_ids"] = torch.tensor(post_tokens["input_ids"])
+            post_tokens["attention_mask"] = torch.tensor(post_tokens["attention_mask"])
+            return post_tokens
         else:
             raise ValueError(f"tokenization_strategy {tokenization_strategy} not supported")
 
@@ -175,17 +155,18 @@ class ClipModel:
                 
                 #t_inputs = self.model.processor(text=texts, return_tensors="pt", padding=True, truncation=True)
                 t_inputs = self.get_tokens(texts, tokenization_strategy)
-                i_inputs = self.model.feature_extractor(images = images_list)
+                i_inputs = self.model.processor(images = images_list, return_tensors="pt", padding=True)
                 
                 for k, v in t_inputs.items():
                     t_inputs[k] = v.to(device)
+                for k, v in i_inputs.items():
+                    i_inputs[k] = v.to(device)
                 
-                pixel_values = torch.tensor(np.array(i_inputs["pixel_values"])).to(device)
                 nums_images = torch.tensor(nums_images).to(dtype=torch.long, device=device)
                 logits, probs = self.model(
-                    input_ids=t_inputs.input_ids,
-                    attention_mask=t_inputs.attention_mask,
-                    pixel_values=pixel_values,
+                    input_ids=t_inputs["input_ids"],
+                    attention_mask=t_inputs["attention_mask"],
+                    pixel_values=i_inputs.pixel_values,
                     nums_images = nums_images,
                 )
 
@@ -198,7 +179,7 @@ class ClipModel:
 
 
 
-    def train(self, train_ds, eval_ds, lr = 5e-5, batch_size= 8, num_epochs = 3, warmup_steps = 0, num_eval_steps = 10, save_path = "./", tokenization_strategy = "first"):
+    def train(self, train_ds, eval_ds, lr = 5e-5, batch_size= 8, num_epochs = 3, warmup_steps = 0, eval_every_epoch= False, num_eval_steps = 10, save_path = "./", tokenization_strategy = "first"):
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.model.to(device)
 
@@ -233,29 +214,27 @@ class ClipModel:
                 nums_images = batch["nums_images"]
 
                 t_inputs = self.get_tokens(texts, tokenization_strategy)
-                i_inputs = self.model.feature_extractor(images = images_list, return_tensors="pt", padding=True)
+                i_inputs = self.model.processor(images = images_list, return_tensors="pt", padding=True)
                 
                 for k, v in t_inputs.items():
                     t_inputs[k] = v.to(device)
-                
-                pixel_values = torch.tensor(np.array(i_inputs["pixel_values"])).to(device)
+                for k, v in i_inputs.items():
+                    i_inputs[k] = v.to(device)
                 labels = torch.tensor(labels).to(device)
 
                 nums_images = torch.tensor(nums_images).to(dtype=torch.long, device=device)
                 outputs = self.model(
-                    input_ids=t_inputs.input_ids,
-                    attention_mask=t_inputs.attention_mask,
-                    pixel_values=pixel_values,
+                    input_ids=t_inputs["input_ids"],
+                    attention_mask=t_inputs["attention_mask"],
+                    pixel_values=i_inputs.pixel_values,
                     nums_images = nums_images,
                 )
-
                 
-                #logits = outputs[0]
+                logits = outputs[0]
                 
-                #preds = torch.argmax(logits, dim=1).detach().cpu().numpy()
-                """
+                preds = torch.argmax(logits, dim=1).detach().cpu().numpy()
                 loss = criterion(logits, labels)
-                if (current_step % num_eval_steps == 0):
+                if (current_step % num_eval_steps == 0 and eval_every_epoch == False):
                     print("Epoch: ", epoch)
                     print("Loss: ", loss.item())
                     eval_metrics = self.eval(eval_ds, tokenization_strategy, batch_size=batch_size)
@@ -267,15 +246,26 @@ class ClipModel:
                         torch.save(self.model.state_dict(), os.path.join(save_path, "best_model.pth"))
                     print("Best metric: ", best_metric)
                     self.model.train()
-                """
-                metrics = compute_metrics(batch["label"], outputs)
-                print(metrics)
-                #loss.backward()
-                #optimizer.step()
-                #scheduler.step()
-                #optimizer.zero_grad()
-                progress_bar.update(1)
+
                 
+
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                progress_bar.update(1)
+            if eval_every_epoch:
+                print("Epoch: ", epoch)
+                print("Loss: ", loss.item())
+                eval_metrics = self.eval(eval_ds, tokenization_strategy, batch_size=batch_size)
+                print("Eval metrics: ", eval_metrics)
+                f1_score = eval_metrics["f1"]
+                if f1_score > best_metric:
+                    print("New best model found")
+                    best_metric = f1_score
+                    torch.save(self.model.state_dict(), os.path.join(save_path, "best_model.pth"))
+                print("Best metric: ", best_metric)
+                self.model.train()
         
         return self.model
     
@@ -283,18 +273,3 @@ class ClipModel:
         self.model.load_state_dict(torch.load(path))
         return self.model
                 
-
-"""
-model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
-
-
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
-
-outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
-"""
