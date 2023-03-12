5378840c831a8969836cd50af7f8fc382996ae69
Lorediel <ld1@live.it>
2023-03-12 15:00:13 +0100

    fix get_tokens

--- only_text/myBert.py
+++ only_text/myBert.py
@@ -73,7 +73,7 @@ class BertModel():
                 tl = len(token_list)
                 if tl>max_len:
                     new_tokens = token_list[:half_len] + token_list[-half_len:]
-                    new_tokens[-1] = [103]
+                    new_tokens[-1] = 103
                     attention_mask = [1] * max_len
                 elif tl<=max_len:
                     # add padding
